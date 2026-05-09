edd4f9ab8ccdee824fafb655ddd0d27573b10b27
harvindsokhal <harvindsokhal@gmail.com>
2026-05-09 18:48:04 +0100

    Add static problem list

--- cmd/lc75/main.go
+++ cmd/lc75/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/harvindsokhal/leetcode-75-go/internal/problems"
+)
 
 func main() {
-	fmt.Println("Leetcode 75 CLI")
+	fmt.Println("Leetcode 75 Problems")
+	fmt.Println("--------------------")
+
+	for _, problem := range problems.All() {
+		fmt.Printf("%03d. %s [%s] - %s\n",
+			problem.Number,
+			problem.Title,
+			problem.Difficulty,
+			problem.Category,
+		)
+	}
 }
--- internal/problems/list.go
+++ internal/problems/list.go
@@ -16,4 +16,32 @@ var LeetCode75 = []Problem{
 		Difficulty: "Easy",
 		Category:   "Array / String",
 	},
+	{
+		Number:     2,
+		Title:      "Greatest Common Divisor of Strings",
+		Slug:       "greatest-common-divisor-of-strings",
+		Difficulty: "Easy",
+		Category:   "Array / String",
+	},
+	{
+		Number:     3,
+		Title:      "Kids With the Greatest Number of Candies",
+		Slug:       "kids-with-the-greatest-number-of-candies",
+		Difficulty: "Easy",
+		Category:   "Array / String",
+	},
+}
+
+func All() []Problem {
+	return LeetCode75
+}
+
+func FindBySlug(slug string) (Problem, bool) {
+	for _, problem := range LeetCode75 {
+		if problem.Slug == slug {
+			return problem, true
+		}
+	}
+
+	return Problem{}, false
 }
